c11900d8abd6dbc5ea71e39fa0ebb99efe4e5a0b
quinn5moli <quinn.moli@gmail.com>
2021-10-30 12:59:04 -0700

    [GET]/api/posts router done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,23 @@
+// const router = express.Router();
+// const express= require('express');
+const router = require('express').Router();
+
+const Posts = require('./posts-model');
+
 // implement your posts router here
+
+router.get('/', async (req, res) => {
+    try {
+        console.log('started')
+        const posts = await Posts.find()
+
+        console.log('finished', posts)
+
+    } catch (error){
+            res.status(500).json({ message: "The posts information could not be retrieved" })
+    }
+})
+
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,15 +1,32 @@
 // implement your server here
 const express = require('express');
 const server = express();
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
 const model = require('./posts/posts-model');
 require('colors');
 
+// require your posts router and connect it here
+
+const postRoutes = require('./posts/posts-router');
+
 //middleware
-server.use(cors());
 server.use(helmet());
 server.use(morgan('dev'));
+server.use(cors());
 server.use(express.json());
 
-// require your posts router and connect it here
+//use post router
+server.use('/api/posts', postRoutes);
+
+//sanity test for ThunderClient
+server.get('/', (req, res) => {
+    res.status(200).json({
+        status: 200,
+        message: 'API is running',
+        time: new Date().toLocaleTimeString(),
+    })
+})
 
 module.exports = server;
