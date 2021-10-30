91f9a1174e54c2628f538d45ab961ddfe70603ae
quinn5moli <quinn.moli@gmail.com>
2021-10-30 11:23:04 -0700

    server built and running; middleware added

--- api/server.js
+++ api/server.js
@@ -1,2 +1,10 @@
 // implement your server here
+const express = require('express');
+const server = express();
+const model = require('./posts/posts-model');
+require('colors');
+
+server.use(express.json());
 // require your posts router and connect it here
+
+module.exports = server;
