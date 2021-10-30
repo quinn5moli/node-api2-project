20803d3be41bd6ded40e0d373c327b55b29cd49d
quinn5moli <quinn.moli@gmail.com>
2021-10-30 11:23:47 -0700

    server built and running; middleware added-forgot to save for last commit

--- api/server.js
+++ api/server.js
@@ -4,7 +4,12 @@ const server = express();
 const model = require('./posts/posts-model');
 require('colors');
 
+//middleware
+server.use(cors());
+server.use(helmet());
+server.use(morgan('dev'));
 server.use(express.json());
+
 // require your posts router and connect it here
 
 module.exports = server;
