b82adf27737ef24ff88bbaaa4680a6be145eba25
quinn5moli <quinn.moli@gmail.com>
2021-10-30 13:33:07 -0700

    [GET]/posts/:id route built

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -18,6 +18,17 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+        console.log('start')
+        const posts = await Posts.findById(req.params.id)
+        if(!posts) return res.status(404).json({ message: "The post with the specified ID does not exist" })
+        res.status(200).json(posts)
+        console.log('finished', posts)
+    } catch (error) {
+            res.status(500).json({ message: "The post information could not be retrieved" })
+    }
+})
 
 
 module.exports = router;
