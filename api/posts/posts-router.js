9baea679491a85d6df83f9c8d9e2d1cb3242f95e
quinn5moli <quinn.moli@gmail.com>
2021-10-31 15:30:43 -0700

    [DELETE] /api/posts/:id rought draft made

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -31,26 +31,47 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-router.post('/', async (req, res) => {
-    try {
-        const addPost = await Posts.insert(req.body)
-        if(!req.body.title || !req.body.contents) return res.status(400).json({message: 'Please provide title and contents for the post'})
-        else res.status(201).json(addPost)
-    } catch (error) {
-        res.status(500).json({ message: "There was an error while saving the post to the database" })
-    }
-    // if(!req.body.title || !req.body.contents) {
-    //     res.status(400).json({ message: "Please provide title and contents for the post" })
-    // } else {
-    //     Posts.insert(req.body)
-    //         .then(post => {
-    //             res.status(201).json(post)
-    //         })
-    //         .catch(() => {
-    //             res.status(500).json({ message: "There was an error while saving the post to the database" })
-    //         })
+router.post('/', (req, res) => {
+    // try {
+    //     const addPost = await Posts.insert(req.body)
+    //     if(!req.body.title || !req.body.contents) return res.status(400).json({message: 'Please provide title and contents for the post'})
+    //     else res.status(201).json(addPost)
+    // } catch (error) {
+    //     res.status(500).json({ message: "There was an error while saving the post to the database" })
     // }
+    if(!req.body.title || !req.body.contents) {
+        res.status(400).json({ message: "Please provide title and contents for the post" })
+    } else {
+        Posts.insert(req.body)
+            .then(post => {
+                res.status(201).json(post, {id:123})
+            })
+            .catch(() => {
+                res.status(500).json({ message: "There was an error while saving the post to the database" })
+            })
+    }
 })
 
+router.put(':id', async (req, res) => {
+    try {
+        const changes = req.body
+        const updated = await Posts.update(req.params.id, changes)
+        if(!id) return res.status(404).json({ message: 'The post with the specified ID does not exist'})
+        if(!req.body.title || !req.body.contents) return res.status(400).json({ message: 'Please provide title and contents for the post'})
+        res.status(200).json(updated)
+    } catch (error) {
+        res.status(500).json ({ message: 'The post information could not be modified'})
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletePost = await Posts.remove(req.params.id)
+        if (!deletePost) return res.status(404).json({ message: 'The post with the specified ID does not exist' })
+        res.status(200).json(deletePost)
+    } catch (error) {
+        res.status(500).json({ message: 'The post could not be removed'})
+    }
+})
 
 module.exports = router;
