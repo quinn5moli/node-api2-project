91324f0582d7fd1b90c00ab6a5cf49d0e7599ec9
quinn5moli <quinn.moli@gmail.com>
2021-11-01 17:26:55 -0700

    tests for [DELETE] passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -63,10 +63,13 @@ router.put(':id', (req, res) => {
 
 router.delete('/:id', async (req, res) => {
     try {
-        const deletePost = await Posts.remove(req.params.id)
-        if (!deletePost) return res.status(404).json({ message: 'The post with the specified ID does not exist' })
-        res.status(200).json(deletePost)
-    } catch (error) {
+        const post = await Posts.findById(req.params.id)
+    if(!post) { res.status(404).json({ 
+        message: 'The post with the specified ID does not exist' })
+    } else {
+        await Posts.remove(req.params.id)
+        res.json(post)
+    }} catch (error) {
         res.status(500).json({ message: 'The post could not be removed'})
     }
 })
